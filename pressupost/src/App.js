d8a5a3938c337db00e271d47b49f32e762274ba9
SaraPlanas <sarakne@gmail.com>
2022-01-14 16:40:00 +0100

    coomit 2

--- pressupost/src/App.js
+++ pressupost/src/App.js
@@ -1,68 +1,80 @@
-
 import './App.css';
 import React from "react";
 
+//components
+import Panell from "./components/inicio/Panell";
 
 
+const { useState } = React;
+
+function App  ()  {
+    const items = [{
+            Id: 1,
+            Name: "Una pàgina web",
+            value: 500,
+        },
+        {
+            Id: 2,
+            Name: "Una consultoria SEO",
+            value: 300,
+        },
+        {
+            Id: 3,
+            Name: "Una campanya de Google Ads web",
+            value: 200,
+        },
+    ]
 
 
-function App() {
-  return (
-    <div className="App">
-      
-       <div> ¿Que quiere hacer?
-         
+    const [checked, setChecked] = useState([]);
+    const [hidePanel, setHidePanel] = useState(true);
+    const handleChange = (item, checked) =>
+{
+if (checked){
 
-         <table>
-         <tr>
-           <td>
-             <input type="checkbox"/>
-             </td>
+        setChecked((prev) => [...prev, item])
+        setHidePanel(false)
 
-         <td>
-           <p>Una pàgina web</p>
-           </td>
-           <td>
-             500 €
-           </td>
-         </tr>
-         <tr>
-           <td>
-             <input type="checkbox"/>
-             </td>
-             <td>
-               <p>Una consultoria SEO </p>
-               </td>
-               <td>
-             300 €
-           </td>
-               </tr>
-           <tr>
-           <td>
-             <input type="checkbox"/>
-             </td>
-             <td>
-               <p>Una campanya de Google Ads web</p>
-               </td>
-               <td>
-             200 €
-           </td>
-               </tr>
-                  </table>
-       </div>
-
-       <table>
-         <tr>
-         <td>
-           <p>Preu</p>
-           </td>
-           <td> XXX
-             </td>
-         </tr>
-       </table>
-      </div>
-    
-  );
+}else{
+        setChecked((prev) =>
+            prev.filter((c) => c.Id !== item.Id && c.value != item.value)
+        );
+        setHidePanel(true)
 }
+}
+    return ( 
+    
+    <div>
+        <h1> ¿Que quiere hacer ? </h1>
+
+        <div> {
+            items.map((c) => {
+
+                if (c.Id === 1){
+                    return ( <div>
+                        <input type = "checkbox" onChange = {
+                            (e) => handleChange(c, e.target.checked) }
+                        /> { ` ${c.Name} ${c.value}`}
+                            <Panell key="panel" showPanel={hidePanel}/>
+                        </div> 
+
+                        
+                    );
+                }else{
+                    return ( <div>
+                        <input type = "checkbox" onChange = {
+                            (e) => handleChange(c, e.target.checked) }
+                        /> { ` ${c.Name} ${c.value}`}
+    
+                        </div> 
+                    );
+                }
+
+            })
+        } <p> Preu : { checked.reduce((sum, { value }) => sum + value, 0) } </p> 
+        </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ pressupost/src/components/inicio/Panell.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled,{css}from'styled-components'
+
+const Panel = styled.div`
+border-radius: 10px;
+border: 2px solid black;
+color: black;
+padding: 1em;
+width: 400px;
+margin:15px;
+`;
+const Tabla = styled.table`
+color: black;
+`;
+
+function Panell(props){
+  
+    return (
+  <Panel hidden={props.showPanel}>
+    <Tabla>
+         <tr>
+           <td>
+            Número de páginas
+             </td>
+            <td>
+              <input type ="number"></input>
+            </td>
+          </tr>
+         <tr>
+           <td>
+            Número de idiomas
+             </td>
+             <td>
+             <input type ="number"></input>
+               </td>
+         </tr>
+         
+         </Tabla>
+       </Panel>
+
+      )
+
+}
+export default Panell;
